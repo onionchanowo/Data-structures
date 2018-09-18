80ffad570694a876fc7a04c87ac8ab66b40dd17b
onionchanowo <188869754@qq.com>
2018-09-18 17:52:58 +0800

    bst

---
+++ BST/C ++/cmake-build-debug/CMakeFiles/AVLTree__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AVLTree__.dir/main.cpp.o"
+  "AVLTree__.pdb"
+  "AVLTree__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AVLTree__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
